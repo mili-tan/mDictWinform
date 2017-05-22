c97cb21c8782d1da17d4af7688bddccfd3e24317
Milkey Tan <819774989@qq.com>
2017-05-22 21:09:12 +0800

    更改字体

--- mDictWFM/Form1.Designer.cs
+++ mDictWFM/Form1.Designer.cs
@@ -57,16 +57,15 @@
             // 
             // labelAbout
             // 
-            this.labelAbout.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.labelAbout.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelAbout.AutoEllipsis = true;
             this.labelAbout.BackColor = System.Drawing.Color.Transparent;
             this.labelAbout.Font = new System.Drawing.Font("Noto Sans CJK SC Regular", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.labelAbout.ForeColor = System.Drawing.Color.Silver;
-            this.labelAbout.Location = new System.Drawing.Point(-2, 424);
+            this.labelAbout.Location = new System.Drawing.Point(-2, 444);
             this.labelAbout.Name = "labelAbout";
-            this.labelAbout.Size = new System.Drawing.Size(715, 36);
+            this.labelAbout.Size = new System.Drawing.Size(734, 36);
             this.labelAbout.TabIndex = 0;
             this.labelAbout.Text = "数据来源 : Bing / Youdao";
             this.labelAbout.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -79,7 +78,7 @@
             this.btnSearch.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnSearch.Depth = 0;
             this.btnSearch.Icon = null;
-            this.btnSearch.Location = new System.Drawing.Point(644, 77);
+            this.btnSearch.Location = new System.Drawing.Point(663, 77);
             this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.btnSearch.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnSearch.Name = "btnSearch";
@@ -106,7 +105,7 @@
             this.wordText.SelectedText = "";
             this.wordText.SelectionLength = 0;
             this.wordText.SelectionStart = 0;
-            this.wordText.Size = new System.Drawing.Size(625, 33);
+            this.wordText.Size = new System.Drawing.Size(644, 33);
             this.wordText.TabIndex = 2;
             this.wordText.TabStop = false;
             this.wordText.UseSystemPasswordChar = false;
@@ -118,11 +117,11 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelWord.AutoSize = true;
-            this.labelWord.Font = new System.Drawing.Font("Noto Sans CJK SC Regular", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelWord.Font = new System.Drawing.Font("Noto Sans CJK SC Regular", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.labelWord.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.labelWord.Location = new System.Drawing.Point(8, 123);
+            this.labelWord.Location = new System.Drawing.Point(5, 122);
             this.labelWord.Name = "labelWord";
-            this.labelWord.Size = new System.Drawing.Size(27, 44);
+            this.labelWord.Size = new System.Drawing.Size(33, 56);
             this.labelWord.TabIndex = 3;
             this.labelWord.Text = " ";
             // 
@@ -132,10 +131,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelEp.AutoSize = true;
-            this.labelEp.Font = new System.Drawing.Font("Noto Sans CJK SC Regular", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.labelEp.Location = new System.Drawing.Point(13, 169);
+            this.labelEp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEp.Location = new System.Drawing.Point(13, 178);
             this.labelEp.Name = "labelEp";
-            this.labelEp.Size = new System.Drawing.Size(13, 22);
+            this.labelEp.Size = new System.Drawing.Size(12, 18);
             this.labelEp.TabIndex = 4;
             this.labelEp.Text = " ";
             // 
@@ -146,136 +145,126 @@
             // 
             // Divider1
             // 
-            this.Divider1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.Divider1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Divider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.Divider1.Depth = 0;
             this.Divider1.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.Divider1.Location = new System.Drawing.Point(0, 424);
+            this.Divider1.Location = new System.Drawing.Point(0, 444);
             this.Divider1.MouseState = MaterialSkin.MouseState.HOVER;
             this.Divider1.Name = "Divider1";
-            this.Divider1.Size = new System.Drawing.Size(715, 1);
+            this.Divider1.Size = new System.Drawing.Size(734, 1);
             this.Divider1.TabIndex = 5;
             this.Divider1.Text = "materialDivider1";
             // 
             // labelPos1
             // 
-            this.labelPos1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.labelPos1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelPos1.AutoEllipsis = true;
             this.labelPos1.Font = new System.Drawing.Font("Noto Sans CJK SC Regular", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.labelPos1.Location = new System.Drawing.Point(12, 210);
+            this.labelPos1.Location = new System.Drawing.Point(12, 230);
             this.labelPos1.Name = "labelPos1";
-            this.labelPos1.Size = new System.Drawing.Size(687, 21);
+            this.labelPos1.Size = new System.Drawing.Size(706, 21);
             this.labelPos1.TabIndex = 6;
             this.labelPos1.Tag = "";
             this.labelPos1.Text = " ";
             // 
             // Divider2
             // 
-            this.Divider2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.Divider2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Divider2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.Divider2.Depth = 0;
             this.Divider2.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.Divider2.Location = new System.Drawing.Point(-2, 202);
+            this.Divider2.Location = new System.Drawing.Point(-2, 222);
             this.Divider2.MouseState = MaterialSkin.MouseState.HOVER;
             this.Divider2.Name = "Divider2";
-            this.Divider2.Size = new System.Drawing.Size(715, 1);
+            this.Divider2.Size = new System.Drawing.Size(734, 1);
             this.Divider2.TabIndex = 7;
             this.Divider2.Text = "materialDivider2";
             // 
             // labelMn1
             // 
-            this.labelMn1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.labelMn1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelMn1.AutoEllipsis = true;
             this.labelMn1.Font = new System.Drawing.Font("Noto Sans CJK SC Regular", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.labelMn1.Location = new System.Drawing.Point(10, 231);
+            this.labelMn1.Location = new System.Drawing.Point(10, 251);
             this.labelMn1.Name = "labelMn1";
-            this.labelMn1.Size = new System.Drawing.Size(691, 30);
+            this.labelMn1.Size = new System.Drawing.Size(710, 30);
             this.labelMn1.TabIndex = 8;
             this.labelMn1.Text = " ";
             // 
             // labelMn2
             // 
-            this.labelMn2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.labelMn2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelMn2.AutoEllipsis = true;
             this.labelMn2.Font = new System.Drawing.Font("Noto Sans CJK SC Regular", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.labelMn2.Location = new System.Drawing.Point(10, 282);
+            this.labelMn2.Location = new System.Drawing.Point(10, 302);
             this.labelMn2.Name = "labelMn2";
-            this.labelMn2.Size = new System.Drawing.Size(691, 30);
+            this.labelMn2.Size = new System.Drawing.Size(710, 30);
             this.labelMn2.TabIndex = 10;
             this.labelMn2.Text = " ";
             // 
             // labelPos2
             // 
-            this.labelPos2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.labelPos2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelPos2.AutoEllipsis = true;
             this.labelPos2.Font = new System.Drawing.Font("Noto Sans CJK SC Regular", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.labelPos2.Location = new System.Drawing.Point(12, 261);
+            this.labelPos2.Location = new System.Drawing.Point(12, 281);
             this.labelPos2.Name = "labelPos2";
-            this.labelPos2.Size = new System.Drawing.Size(687, 21);
+            this.labelPos2.Size = new System.Drawing.Size(706, 21);
             this.labelPos2.TabIndex = 9;
             this.labelPos2.Text = " ";
             // 
             // labelMn3
             // 
-            this.labelMn3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.labelMn3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelMn3.AutoEllipsis = true;
             this.labelMn3.Font = new System.Drawing.Font("Noto Sans CJK SC Regular", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.labelMn3.Location = new System.Drawing.Point(10, 333);
+            this.labelMn3.Location = new System.Drawing.Point(10, 353);
             this.labelMn3.Name = "labelMn3";
-            this.labelMn3.Size = new System.Drawing.Size(691, 30);
+            this.labelMn3.Size = new System.Drawing.Size(710, 30);
             this.labelMn3.TabIndex = 12;
             this.labelMn3.Text = " ";
             // 
             // labelPos3
             // 
-            this.labelPos3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.labelPos3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelPos3.AutoEllipsis = true;
             this.labelPos3.Font = new System.Drawing.Font("Noto Sans CJK SC Regular", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.labelPos3.Location = new System.Drawing.Point(12, 312);
+            this.labelPos3.Location = new System.Drawing.Point(12, 332);
             this.labelPos3.Name = "labelPos3";
-            this.labelPos3.Size = new System.Drawing.Size(687, 21);
+            this.labelPos3.Size = new System.Drawing.Size(706, 21);
             this.labelPos3.TabIndex = 11;
             this.labelPos3.Text = " ";
             // 
             // labelMn4
             // 
-            this.labelMn4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.labelMn4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelMn4.AutoEllipsis = true;
             this.labelMn4.Font = new System.Drawing.Font("Noto Sans CJK SC Regular", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.labelMn4.Location = new System.Drawing.Point(10, 384);
+            this.labelMn4.Location = new System.Drawing.Point(10, 404);
             this.labelMn4.Name = "labelMn4";
-            this.labelMn4.Size = new System.Drawing.Size(689, 30);
+            this.labelMn4.Size = new System.Drawing.Size(708, 30);
             this.labelMn4.TabIndex = 14;
             this.labelMn4.Text = " ";
             // 
             // labelPos4
             // 
-            this.labelPos4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.labelPos4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelPos4.AutoEllipsis = true;
             this.labelPos4.Font = new System.Drawing.Font("Noto Sans CJK SC Regular", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.labelPos4.Location = new System.Drawing.Point(12, 363);
+            this.labelPos4.Location = new System.Drawing.Point(12, 383);
             this.labelPos4.Name = "labelPos4";
-            this.labelPos4.Size = new System.Drawing.Size(687, 21);
+            this.labelPos4.Size = new System.Drawing.Size(706, 21);
             this.labelPos4.TabIndex = 13;
             this.labelPos4.Text = " ";
             // 
@@ -296,22 +285,22 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.windowsMediaPlayer.Enabled = true;
-            this.windowsMediaPlayer.Location = new System.Drawing.Point(12, 438);
+            this.windowsMediaPlayer.Location = new System.Drawing.Point(509, 122);
             this.windowsMediaPlayer.Name = "windowsMediaPlayer";
             this.windowsMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("windowsMediaPlayer.OcxState")));
-            this.windowsMediaPlayer.Size = new System.Drawing.Size(111, 10);
+            this.windowsMediaPlayer.Size = new System.Drawing.Size(209, 44);
             this.windowsMediaPlayer.TabIndex = 16;
             this.windowsMediaPlayer.Visible = false;
             // 
             // btnCopy
             // 
-            this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCopy.AutoSize = true;
             this.btnCopy.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnCopy.Depth = 0;
             this.btnCopy.Icon = global::mDictWFM.Properties.Resources.copy;
-            this.btnCopy.Location = new System.Drawing.Point(580, 155);
+            this.btnCopy.Location = new System.Drawing.Point(601, 175);
             this.btnCopy.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.btnCopy.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnCopy.Name = "btnCopy";
@@ -324,13 +313,13 @@
             // 
             // btnSpeech
             // 
-            this.btnSpeech.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.btnSpeech.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnSpeech.AutoSize = true;
             this.btnSpeech.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnSpeech.Depth = 0;
             this.btnSpeech.Icon = global::mDictWFM.Properties.Resources.text_to_speech;
-            this.btnSpeech.Location = new System.Drawing.Point(488, 155);
+            this.btnSpeech.Location = new System.Drawing.Point(509, 175);
             this.btnSpeech.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.btnSpeech.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnSpeech.Name = "btnSpeech";
@@ -350,7 +339,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 460);
+            this.ClientSize = new System.Drawing.Size(730, 480);
             this.Controls.Add(this.btnCopy);
             this.Controls.Add(this.windowsMediaPlayer);
             this.Controls.Add(this.btnSpeech);
